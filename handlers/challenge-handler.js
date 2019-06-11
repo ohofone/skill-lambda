b6626613fc2a32f4cf31594583febb108f17e55f
Jesse Van Volkinburg <vanvoljg@gmail.com>
2019-06-11 16:03:43 -0700

    update with latest code (from lambda online editor)

--- handlers/challenge-handler.js
+++ handlers/challenge-handler.js
@@ -1,12 +1,12 @@
-"use strict";
+'use strict';
 
-const superagent = require("superagent");
+const superagent = require('superagent');
 
 module.exports = {
   canHandle(handlerInput) {
     return (
-      handlerInput.requestEnvelope.request.type === "IntentRequest" &&
-      handlerInput.requestEnvelope.request.intent.name === "dataTypeIntent"
+      handlerInput.requestEnvelope.request.type === 'IntentRequest' &&
+      handlerInput.requestEnvelope.request.intent.name === 'dataTypeIntent'
     );
   },
   handle(handlerInput) {
@@ -14,7 +14,17 @@ module.exports = {
     const dataType =
       handlerInput.requestEnvelope.request.intent.slots.dataType.resolutions
         .resolutionsPerAuthority[0].values[0].value.id;
-    const url = `https://oh-of-one.herokuapp.com/${dataType}/anyDifficulty`;
+
+    const difficulty =
+      handlerInput.requestEnvelope.request.intent.slots
+        .difficulty.resolutions ?
+        handlerInput.requestEnvelope.request.intent.slots.difficulty
+          .resolutions.resolutionsPerAuthority[0].values[0].value.id :
+        'anyDifficulty';
+
+    const userID = handlerInput.requestEnvelope.session.user.userId;
+    const url = `https://oh-of-one.herokuapp.com/question/${dataType}/${difficulty}/${userID}`;
+
     return superagent
       .get(url)
       .then(response => {
@@ -22,7 +32,7 @@ module.exports = {
         let speechText = '';
 
         if (response.body[0]) {
-          speechText = response.body[0].question;
+          speechText = 'Here is your question. ' + response.body[0].question;
         }
 
         return handlerInput.responseBuilder
--- index.js
+++ index.js
@@ -1,91 +1,11 @@
 'use strict';
 
-// This sample demonstrates handling intents from an Alexa skill using the Alexa Skills Kit SDK (v2).
-// Please visit https://alexa.design/cookbook for additional examples on implementing slots, dialog management,
-// session persistence, api calls, and more.
 const Alexa = require('ask-sdk-core');
 
 const LaunchRequestHandler = require('./handlers/launch-handler.js');
 const ChallengeIntentHandler = require('./handlers/challenge-handler.js');
-
-const HelloWorldIntentHandler = {
-  canHandle(handlerInput) {
-    return (
-      handlerInput.requestEnvelope.request.type === 'IntentRequest' &&
-      handlerInput.requestEnvelope.request.intent.name === 'HelloWorldIntent'
-    );
-  },
-  handle(handlerInput) {
-    const speechText = 'Hello World!';
-    return (
-      handlerInput.responseBuilder
-        .speak(speechText)
-        //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
-        .getResponse()
-    );
-  },
-};
-const HelpIntentHandler = {
-  canHandle(handlerInput) {
-    return (
-      handlerInput.requestEnvelope.request.type === 'IntentRequest' &&
-      handlerInput.requestEnvelope.request.intent.name === 'AMAZON.HelpIntent'
-    );
-  },
-  handle(handlerInput) {
-    const speechText = 'You can say hello to me! How can I help?';
-
-    return handlerInput.responseBuilder
-      .speak(speechText)
-      .reprompt(speechText)
-      .getResponse();
-  },
-};
-const CancelAndStopIntentHandler = {
-  canHandle(handlerInput) {
-    return (
-      handlerInput.requestEnvelope.request.type === 'IntentRequest' &&
-      (handlerInput.requestEnvelope.request.intent.name ===
-        'AMAZON.CancelIntent' ||
-        handlerInput.requestEnvelope.request.intent.name ===
-        'AMAZON.StopIntent')
-    );
-  },
-  handle(handlerInput) {
-    const speechText = 'Goodbye!';
-    return handlerInput.responseBuilder.speak(speechText).getResponse();
-  },
-};
-const SessionEndedRequestHandler = {
-  canHandle(handlerInput) {
-    return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest';
-  },
-  handle(handlerInput) {
-    // Any cleanup logic goes here.
-    return handlerInput.responseBuilder.getResponse();
-  },
-};
-
-// The intent reflector is used for interaction model testing and debugging.
-// It will simply repeat the intent the user said. You can create custom handlers
-// for your intents by defining them above, then also adding them to the request
-// handler chain below.
-const IntentReflectorHandler = {
-  canHandle(handlerInput) {
-    return handlerInput.requestEnvelope.request.type === 'IntentRequest';
-  },
-  handle(handlerInput) {
-    const intentName = handlerInput.requestEnvelope.request.intent.name;
-    const speechText = `You just triggered ${intentName}`;
-
-    return (
-      handlerInput.responseBuilder
-        .speak(speechText)
-        //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
-        .getResponse()
-    );
-  },
-};
+// const HintIntentHandler = require('./handlers/hint-handler.js');
+// const SolutionIntentHandler = require('./handlers/solution-handler.js');
 
 // Generic error handling to capture any syntax or routing errors. If you receive an error
 // stating the request handler chain is not found, you have not implemented a handler for
@@ -112,11 +32,8 @@ exports.handler = Alexa.SkillBuilders.custom()
   .addRequestHandlers(
     LaunchRequestHandler,
     ChallengeIntentHandler,
-    HelloWorldIntentHandler,
-    HelpIntentHandler,
-    CancelAndStopIntentHandler,
-    SessionEndedRequestHandler,
-    IntentReflectorHandler
-  ) // make sure IntentReflectorHandler is last so it doesn't override your custom intent handlers
+    // HintIntentHandler,
+    // SolutionIntentHandler,
+  )
   .addErrorHandlers(ErrorHandler)
   .lambda();
