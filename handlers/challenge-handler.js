5e1906088c706989c9ccd316cc19a9c79d60e5c4
Jesse Van Volkinburg <vanvoljg@gmail.com>
2019-06-10 17:04:45 -0700

    update challenge handler

--- handlers/challenge-handler.js
+++ handlers/challenge-handler.js
@@ -11,12 +11,16 @@ module.exports = {
   },
   handle(handlerInput) {
     // const speechText = 'What kind of challenge are you looking for?';
-    const dataType = handlerInput.requestEnvelope.request.intent.slots.dataType.id;
+    const dataType =
+      handlerInput.requestEnvelope.request.intent.slots.dataType.resolutions
+        .resolutionsPerAuthority[0].values[0].value.name;
     // const speechText =
     //   handlerInput.requestEnvelope.request.intent.slots.dataType.value;
     const url = `https://oh-of-one.herokuapp.com/${dataType}/anyDifficulty`;
-    superagent.get(url).then(response => {
-      const speechText = response.body[0].question;
+    superagent
+      .get(url)
+      .then(response => {
+        const speechText = response.body[0].question;
         return handlerInput.responseBuilder
           .reprompt("Let me know when you're done")
           .speak(speechText)
